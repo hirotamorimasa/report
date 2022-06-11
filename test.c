a0d5a209e39f926c99431433cc3f3ff28de38a2e
Hiromasa Morita <morimasahirota>
2022-06-11 15:13:45 +0900

    Updata

--- test.c
+++ test.c
@@ -1,9 +1,13 @@
-#include <./sub.city011/city011.h>
-#include <./sub.city012/city012.h>
-#include <./sub.city021/city021.h>
-#include <./sub.city022/city022.h>
+#include "./sub.city011/city011.h"
+#include "./sub.city012/city012.h"
+#include "./sub.city021/city021.h"
+#include "./sub.city022/city022.h"
 
 int main(void)
 {
+	city011_main();
+	city012_main();
+	city021_main();
+	city022_main();
 	return 0;
 }
